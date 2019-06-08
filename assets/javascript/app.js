85e415f49a75b491f46ee8373dd5f1fefa54a1d1
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-08 16:56:41 -0700

    added jquery link, new style on css, added background image in images, messing around with js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,73 @@
-// Set timer 
+$(document).ready(function () {
 
-let timeElm = document.getElementById('time-left');
-let timer = function(x){
-  if (x === 0) {
-    return;
-  }
-  timeElm.innerHTML = x;
-  return setTimeout(() => {timer(--x)}, 1000)
-}
+  // Declaring variables 
 
-timer(30);
+  var userPick = [];
 
-// after 30 seconds, execute timeUP
+  var correct = 0;
+  var incorrect = 0;
+  var missed = 0;
+  var questionCounter = 0;
+  var images;
 
-setTimeout(timeUp, 1000 * 30);
+  var triviaQuestions = [{
+
+      question: "How many Grand Slams doeo Rodger Federe have?",
+      choices: [8, 15, 23, 20],
+      images: [""],
+      validAnswer: 3
+    },
+    {
+      question: "How sets are in a tennis match?",
+      choices: [4, 3, 2, 5],
+      images: [""],
+      validAnswer: 1
+
+    },
+    {
+      question: "How many French Opens has Rafa Nadal won?",
+      choices: [8, 10, 2, 5],
+      images: [""],
+      validAnswer: 1
+
+    },
+    {
+      question: "What was Andy Murray's highest ranking?",
+      choices: [3, 5, 1, 10],
+      images: [""],
+      validAnswer: 2
+    }
+
+  ];
 
 
-function timeUp() {
-  // in the element with an id of time-left add an h2 saying Time's Up!
-  // console log done
-  console.log("done");
-  $("#time-left").append("<h2>Time's Up!</h2>");
-  console.log("time is up");
+  // start game 
 
-}
+  $("#start-button").on("click", function () {
+    
+
+  })
+
+  $("#time-left").append()
+  setTimeout(function () {
+   // Set timer 
+
+   // after 30 seconds, execute timeUP
+
+   setTimeout(timeUp, 1000 * 30);
+
+
+   function timeUp() {
+     // in the element with an id of time-left add an h2 saying Time's Up!
+     // console log done
+     console.log("done");
+     $("#time-left").append("<h2>Time's Up!</h2>");
+     console.log("time is up");
+
+   }
+
+ })
+
+
+
+})
