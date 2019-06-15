dd4cb7ccc4b1a97bd5609d75c6b44fcb949d2ca2
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-14 22:42:10 -0700

    new index html layout, new js code beside questions, new css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,163 +1,117 @@
-// declaring variables
+  // TRIVIA GAME 
 
-// store the html layout 
-var displayTrivia = [];
+  var correctAns = 0;
+  var incorrectAns = 0;
+  var timeLeft = 30;
+  var intervalID;
+  var indexQandA = 0;
+  var answered = false;
+  var correct;
 
-  // arrary for user Answers  
-  var userAns = [];
-  var answers = [];
+  $(document).ready(function () {
 
-// user's correct answers 
-var correct = 0;
-var incorrect = 0;
-
-
-var triviaContainer = document.getElementById("triviaContainer");
-var resultsContainer = document.getElementById("results");
-
-var submitButton = document.getElementById("submit");
-
-
-
-
-$(document).ready(function () {
-
-  // Trivia Questions 
-  var tennisQuestions = [{
-      question: "How many Grand Slams doeo Rodger Federe have?",
-      answers: {
-        a: 8,
-        b: 15,
-        c: 23,
-        d: 20
+    var tennisQuestions = [{
+        question: "How many Grand Slams doeo Rodger Federe have?",
+        answers: [8, 15, 23, 20],
+        correct: "3"
       },
-      correctAnswer: "d"
-    },
-    {
-
-      question: "How sets are in a tennis match?",
-      answers: {
-        a: 4,
-        b: 3,
-        c: 2,
-        d: 5
+      {
+        question: "How sets are in a tennis match?",
+        answers: [4, 3, 2, 5],
+        correct: "1"
       },
-      correctAnswer: "b"
+      {
+        question: "How many French Opens has Rafa Nadal won?",
+        answers: [8, 10, 2, 5],
+        correct: "1"
 
-    },
-    {
-      question: "How many French Opens has Rafa Nadal won?",
-      answers: {
-        a: 8,
-        b: 10,
-        c: 2,
-        d: 5
       },
-      correctAnswer: "b"
+      {
 
-    },
-    {
+        question: "What was Andy Murray's highest ranking?",
+        answers: [3, 5, 1, 10],
+        correct: "2"
+      },
+      {
+        question: "Where is Novak Djokovic from?",
+        answers: ["Monico", "Serbia", "Bulgaria", "Romania"],
+        correct: "1"
+      }
+    ];
 
-      question: "What was Andy Murray's highest ranking?",
-      answers: {
-        a: 3,
-        b: 5,
-        c: 1,
-        d: 10
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Where is Novak Djokovic from?",
-      answers: {
-        a: "Monico",
-        b: "Serbia",
-        c: "Bulgaria",
-        d: "Romania"
-      },
-      correctAnswer: "b"
+    // declaring functions 
+
+    function startGame() {
+      console.log("LET'S BEGIN");
+      $("#start-button").remove();
+      correctAns = 0;
+      incorrectAns = 0;
+      loadQandA();
     }
 
-  ];
-
-
-
-  function createTrivia() {
-
-    // store the html layout 
-    var displayTrivia = [];
-    var answers = [];
-
-    tennisQuestions.forEach((currentQuestion, questionNumb) => {
-
-  
-     
-
-      for (letter in tennisQuestions.answers) {
-        // create radio button
-        answers.push(
-          `<label><input type="radio" name="question${questionNumb}" value="${letter}"> ${letter}: ${currentQuestion.answers[letter]}</label>`
-        );
+    function loadQandA() {
+      answered = false;
+      timeLeft = 30;
+      intervalID = setInterval(timer, 1000);
+      if (answered === false) {
+        timer();
+      }
+      correct = tennisQuestions[indexQandA].correct;
+      var question = tennisQuestions[indexQandA].question;
+      $("#question").html(question);
+      for (var i = 0; i < 4; i++) {
+        var answer = tennisQuestions[indexQandA].answers[i];
+        $("#answer").append('<div class=answerAll id=' + i + '>' + answer + '</div>')
       }
 
-      // add question and answer to the trivia display 
-      displayTrivia.push(
-        `<div class="slide"><div class="question">${currentQuestion.question}</div>
-  <div class="answers"> ${answers.join("")}</div></div>`
-      );
+    }
 
+    function timer() {
+      if (timeLeft === 0) {
+        answered = true;
+        clearInterval(intervalID);
+        $("#question").text("THE CORRECT ANSWER IS: " + tennisQuestions[indexQandA].answer[correct]);
+      } else if (answered === true) {
+        clearInterval(intervalID);
+      } else {
+        timeLeft--;
+        $('#time-left').text('YOU HAVE ' + timeLeft + ' SECONDS TO CHOOSE');
+      }
+    }
+
+    function correctAnswer() {
+      correctAns++;
+      restGame();
+    }
+
+    function incorrectAnswer() {
+      incorrectAns++;
+      restGame();
+
+    }
+
+    function resetGame() {
+      $("#answersAll").remove();
+      if (indexQandA < tennisQuestions.length) {
+        setTimeout(function () {
+          loadQandA();
+
+        })
+      } else {
+        setTimeout(function () {
+          $("#question").remove();
+          $("#time-left").remove();
+          $("#answers").remove();
+     
+
+        })
+      }
+    }
+
+
+    $("#start-button").on("click", function () {
+      $("#start-button");
+      startGame();
     });
 
-
-
-    document.getElementById("triviaContainer").innerHTML = displayTrivia.join("");;
-
-  }
-
-
-  function showResults() {
-
-    var answers = triviaContainer.getElementById("answer");
-
-    // user's correct answers 
-    var correct = 0;
-    var incorrect = 0;
-
-    // for each question 
-
-    tennisQuestions.forEach((currentQuestion, questionNumb) => {
-      var answers = answers[questionNumb];
-      var selector = `input[name=question${questionNumb}]:checked`;
-      var userAns = (answers.getElementById(selector) || {}).value;
-
-
-      // if the user answers correctly 
-
-      if (userAns === currentQuestion.correctAnswer) {
-        correct++;
-      } else if (userAns !== currentQuestion.correctAnswer) {
-        // if the answer is incorrect 
-        incorrect++;
-      };
-
-
-    });
-
-    resultsContainer.innerHTML = `${correct} out of ${tennisQuestions.length}`;
-    resultsContainer.innerHTML = `${incorrect} out of ${tennisQuestions.length}`;
-
-  };
-
-  //display the trivia game
-
-  createTrivia();
-
-  var triviaContainer = document.getElementById("trivia");
-  var resultsContainer = document.getElementById("results");
-  var submitButton = document.getElementById("submit");
-  // var answerContainer = document.getElementById("answer");
-
-
-
-
-});
+  });
